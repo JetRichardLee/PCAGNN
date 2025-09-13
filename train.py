9a3f91feebb333f7288927a74dae4fb39c690000
Jiate Li <69561882+JetRichardLee@users.noreply.github.com>
2025-09-13 14:06:01 -0700

    Update train.py

--- train.py
+++ train.py
@@ -29,7 +29,7 @@ train_args = {
     }
 
 All_N = ["computers","photo","Cora-ML","PubMed", "CiteSeer","Airports","club","Wiki"]
-All_G = ["DD","AIDS", "ENZYMES","Facebook", "PROTEINS","IMDB-BINARY","COLLAB","REDDIT-BINARY"]
+All_G = ["DD","AIDS", "ENZYMES","Mutagenicity", "PROTEINS","IMDB-BINARY","COLLAB","REDDIT-BINARY"]
 #Mutagenicity
 align_size = 1024
 hidden_size = 512
@@ -466,4 +466,5 @@ with torch.no_grad():
         val_acc = evaluate(out[valM_G[d]], Ys_G[d][valM_G[d]])
         test_acc = evaluate(out[testM_G[d]], Ys_G[d][testM_G[d]])
         print(f"---{Test_G[d]:<15} Train acc:{train_acc:.4f} Val acc:{val_acc:.4f} Test acc:{test_acc:.4f}")
+
 
