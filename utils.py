6831efbe37f26792a5d848dccdebff573788f91a
Jiate Li <69561882+JetRichardLee@users.noreply.github.com>
2025-09-14 15:52:53 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -97,9 +97,27 @@ def store_checkpoint_test(paper, dataset, Down_N,Down_G):
     for i in range(len(Down_G)):
         checkpoint = {'model_state_dict': Down_G[i].state_dict()}
         torch.save(checkpoint, os.path.join(save_dir, f"best_graph_{i}_model_test"))
+   
+
+def store_checkpoint_down(paper, dataset, Down,Name):
+    """
+    Store the model weights at a predifined location.
+    :param paper: str, the paper 
+    :param dataset: str, the dataset
+    :param model: the model who's parameters we whish to save
+    :param train_acc: training accuracy obtained by the model
+    :param val_acc: validation accuracy obtained by the model
+    :param test_acc: test accuracy obtained by the model
+    :param epoch: the current epoch of the training process
+    :retunrs: None
+    """
+    save_dir = f"./checkpoints/{paper}/{dataset}"
+
+    checkpoint = {'model_state_dict': Down.state_dict()}
+    torch.save(checkpoint, os.path.join(save_dir, f"best_{Name}_model_test"))
         
-        
-def load_best_model_test(paper, dataset, Down_N,Down_G, eval_enabled):
+       
+def load_best_model_test(paper, dataset, Down_N,Down_G,Test_N,Test_G, eval_enabled):
     """
     Load the model parameters from a checkpoint into a model
     :param best_epoch: the epoch which obtained the best result. use -1 to chose the "best model"
@@ -111,12 +129,12 @@ def load_best_model_test(paper, dataset, Down_N,Down_G, eval_enabled):
     """
     
     for i in range(len(Down_N)):
-        checkpoint = torch.load(f"./checkpoints/{paper}/{dataset}/best_node_{i}_model_test")
+        checkpoint = torch.load(f"./checkpoints/{paper}/{dataset}/best_{Test_N[i]}_model_test")
         Down_N[i].load_state_dict(checkpoint['model_state_dict'])
         if eval_enabled: Down_N[i].eval()
     
     for i in range(len(Down_G)):
-        checkpoint = torch.load(f"./checkpoints/{paper}/{dataset}/best_graph_{i}_model_test")
+        checkpoint = torch.load(f"./checkpoints/{paper}/{dataset}/best_{Test_G[i]}_model_test")
         Down_G[i].load_state_dict(checkpoint['model_state_dict'])
         if eval_enabled: Down_G[i].eval()
     
